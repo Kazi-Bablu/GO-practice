2be3ca4a3cc1dcb336d56111bff35c40277fbccf
kazi bablu <bablu@shuttlebd.com>
2022-08-12 00:37:46 +0600

    data type

---
+++ dataType.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var userName
+	var userTicket
+
+	userName = "Tom"
+	userTicket = 2
+	fmt.Printf("User %v booked %v tickets. \n", userName, userTicket)
+}
