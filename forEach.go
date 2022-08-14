65a5d1bef5f1ac28caa9257f247a6721ded73b34
kazi bablu <bablu@shuttlebd.com>
2022-08-14 14:55:45 +0600

    for loop

---
+++ forEach.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var conferenceName = "Go conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	var bookings []string
+
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets)
+	fmt.Println("get your tickets here to attend")
+
+	for {
+
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
+		fmt.Println("Enter number of ticket : ")
+		fmt.Scan(&userTickets)
+
+		remainingTickets = remainingTickets - userTickets
+
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("The Whole slice: %v\n", bookings)
+		fmt.Printf("The first value: %v\n", bookings[0])
+
+		fmt.Printf("Slice type: %T\n", bookings)
+		fmt.Printf("Slice length: %v\n", len(bookings))
+
+		fmt.Printf("Thank you %v %v for booking %v tickets. You will  receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+
+		firstNames:=[]string{}
+
+		for _, booking:= range bookings{
+			var names = strings.Fields(booking)
+			firstName = append(firstNames,names[0])
+		}
+
+		fmt.Println("The first names of bookings are: %v\n",firstName)
+
+	}
+
+}
